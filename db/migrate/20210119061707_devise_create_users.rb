616dcae07b81cd74ffa88873d0028e6a4f064db2
takuro-1011 <taku1011opal@icloud.com>
2021-02-02 16:46:43 +0900

    性別カラム追加法変更

--- db/migrate/20210119061707_devise_create_users.rb
+++ db/migrate/20210119061707_devise_create_users.rb
@@ -11,6 +11,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :phone_number,      null:false
       t.date :birthday,             null: false
       t.text :allergy
+      t.integer :gender,            null:false
       
       
       ## Recoverable
@@ -41,7 +42,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
 
       t.timestamps null: false
     end
-    add_column :users, :gender, :integer,    null: false
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
